db4865083c9fa57544a7ee5d2ffab0af6e55dd2f
Vishal Kumar <57776597+VishyFishy02@users.noreply.github.com>
2024-12-07 01:18:24 -0500

    version two of the website, ready for deployment

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Constants
 const TORUS_PARAMS = { radius: 10, tubeRadius: 3, radialSegments: 16, tubularSegments: 100 };
-const DIAMOND_PARAMS = { count: 200, spread: 100 };
+const DIAMOND_PARAMS = { count: 2000, spread: 400 };
 const BOOK_SCALE = 5;
 const LAPTOP_SCALE = 0.15;
 const SCROLL_SPEED = 0.001;
@@ -41,17 +41,17 @@ function addLight(){
   scene.add(ambientLight);
 }
 
-let torus;
-function addTorus(){
-  const geometry = new THREE.TorusGeometry(TORUS_PARAMS.radius, TORUS_PARAMS.tubeRadius, TORUS_PARAMS.radialSegments, TORUS_PARAMS.tubularSegments);
-  const material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
-  torus = new THREE.Mesh(geometry, material);
-  scene.add(torus);
-}
+// let torus;
+// function addTorus(){
+//   const geometry = new THREE.TorusGeometry(TORUS_PARAMS.radius, TORUS_PARAMS.tubeRadius, TORUS_PARAMS.radialSegments, TORUS_PARAMS.tubularSegments);
+//   const material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+//   torus = new THREE.Mesh(geometry, material);
+//   scene.add(torus);
+// }
 
 
 // --Random Diamonds--
-const diamondGeometry = new THREE.OctahedronGeometry(0.60, 0);
+const diamondGeometry = new THREE.OctahedronGeometry(0.40, 0);
 const diamondMaterial = new THREE.MeshStandardMaterial({color: 0xF08492});
 function addDiamond() {
   const diamond = new THREE.Mesh(diamondGeometry, diamondMaterial);
@@ -61,43 +61,43 @@ function addDiamond() {
 }
 
 
-let book;
-let laptop;
+// let book;
+// let laptop;
 
-function loadModels(){
-  const loader = new GLTFLoader();
+// function loadModels(){
+//   const loader = new GLTFLoader();
 
-  // red book
-  loader.load('./red_book.glb', function(gltf) {
-    book = gltf.scene;
-    book.scale.set(BOOK_SCALE, BOOK_SCALE, BOOK_SCALE);
-    book.position.set(-5, 0.7, 30);
-    scene.add(book);
-  });
+//   // red book
+//   loader.load('./red_book.glb', function(gltf) {
+//     book = gltf.scene;
+//     book.scale.set(BOOK_SCALE, BOOK_SCALE, BOOK_SCALE);
+//     book.position.set(-5, 0.7, 30);
+//     scene.add(book);
+//   });
 
-  // laptop
-  loader.load('./laptop.glb', function(gltf) {
-    laptop = gltf.scene;
-    laptop.scale.set(LAPTOP_SCALE, LAPTOP_SCALE, LAPTOP_SCALE);
-    laptop.position.set(0, 0, -5);
-    scene.add(laptop);
-  });
+//   // laptop
+//   loader.load('./laptop.glb', function(gltf) {
+//     laptop = gltf.scene;
+//     laptop.scale.set(LAPTOP_SCALE, LAPTOP_SCALE, LAPTOP_SCALE);
+//     laptop.position.set(0, 0, -5);
+//     scene.add(laptop);
+//   });
 
-}
+// }
 
 // scroll animation
 function moveCamera(){
   const t = document.body.getBoundingClientRect().top;
-  if(book){
-    book.rotation.x += .005;
-    book.rotation.y += .0075;
-    book.rotation.z += .005;
-  }
+  // if(book){
+  //   book.rotation.x += .005;
+  //   book.rotation.y += .0075;
+  //   book.rotation.z += .005;
+  // }
 
-  if(laptop){
-    laptop.rotation.y = t * 0.001;
-    laptop.rotation.z = t * 0.001;
-  }
+  // if(laptop){
+  //   laptop.rotation.y = t * 0.001;
+  //   laptop.rotation.z = t * 0.001;
+  // }
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
@@ -117,12 +117,12 @@ function onWindowResize() {
 function animate(){
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.001;
-  torus.rotation.y += 0.001;
-  torus.rotation.z += 0.001;
-  if(book){
-    book.rotation.x += 0.0075;
-  }
+  // torus.rotation.x += 0.001;
+  // torus.rotation.y += 0.001;
+  // torus.rotation.z += 0.001;
+  // if(book){
+  //   book.rotation.x += 0.0075;
+  // }
   renderer.render(scene, camera);
 }
 
@@ -132,9 +132,9 @@ const scene = setupScene();
 const camera = setupCamera();
 const renderer = setupRenderer();
 addLight();
-addTorus();
+// addTorus();
 Array(DIAMOND_PARAMS.count).fill().forEach(addDiamond);
-loadModels();
+// loadModels();
 
 document.body.onscroll = moveCamera;
 window.addEventListener('resize', onWindowResize, false);
